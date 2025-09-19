148d31a08c4f9f5ccd1973c49f6e66831b79acfb
Khatchi <solomon9ice2014@gmail.com>
2025-09-19 14:44:10 +0100

    feat(settings.py): Register custom apps=> rest_framework, authentications, inventory, suppliers, reports Configures the postgres database URL and the environmental variales Run the first migrations for the data drf default apps models

--- backend/pharmstock/settings.py
+++ backend/pharmstock/settings.py
@@ -11,21 +11,31 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import sys
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Add apps directory to Python path
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
+
+# Load environment variables from a .env file
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%^lvk4qa$w=&0$ybzrfu^gs=@d*+5dn#s6mxqmw2smv7v9dgv!'
+SECRET_KEY=env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -37,6 +47,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    'rest_framework',
+    'drf_yasg',
+    'apps.inventory',
+    'apps.authentication',
+    'apps.suppliers',
+    'apps.reports',
 ]
 
 MIDDLEWARE = [
@@ -72,10 +89,21 @@ WSGI_APPLICATION = 'pharmstock.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
